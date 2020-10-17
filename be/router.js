dc800cf3cc543d3249606471b72a4360badf5169
Yasin <yasinvural0@gmail.com>
2020-10-18 00:07:54 +0300

    routers are added

--- be/router.js
+++ be/router.js
@@ -6,7 +6,8 @@ router.get("/", (req, res) => {
   res.status(200).send("Hello Weather App !!");
 });
 
-router.get("/cities", (req, res) => {
+router.get("/api/cities", (req, res) => {
+  // TODO: get cities from db
   const cities = [
     { id: 1, name: "Tokio" },
     { id: 2, name: "London" },
@@ -16,6 +17,23 @@ router.get("/cities", (req, res) => {
     { id: 6, name: "Stockholm" },
   ];
   res.status(200).send(cities);
+});
+
+router.get("/api/weather", (req, res) => {
+  const { id } = req.query;
+  // TODO: get detail from db
+  if (id) {
+    const weatherDetail = {
+      id: 1,
+      name: "Tokio",
+      value: 24,
+      img: "sunny",
+      desc: "Clear Sky",
+    };
+    res.status(200).send(weatherDetail);
+  } else {
+    throw new Error("not found");
+  }
 });
 
 module.exports = router;
